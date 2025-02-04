8ec9698cf896cb9876b57f1d665f584fb824f056
RAYTHARY\ghfjt <ghfjtorl121291@naver.com>
2025-02-04 15:16:55 +0900

    0204

--- Assets/Script/Player/AutoTarget.cs
+++ Assets/Script/Player/AutoTarget.cs
@@ -21,7 +21,7 @@ public class AutoTarget : MonoBehaviour
     void Update()
     {
         if (autoTarget)
-        {
+        {   
             target.position = FindAutoTarget();
         }
         else
@@ -34,17 +34,6 @@ public class AutoTarget : MonoBehaviour
     //제일가까운적을  리턴해줌
     private Vector3 FindAutoTarget()
     {
-        //나중에 지워줄것(완성후) 
-        #region
-        //Enemy enemy = Enemy.enemyActiveList
-        //   .OrderBy(enemy => Vector3.Distance(a.position, enemy.transform.position)) // 거리 기준으로 정렬
-        //   .FirstOrDefault(); // 가장 가까운 적 반환 (리스트가 비어있으면 null 반환)
-        //if (enemy == null)
-        //    return null;
-        //else if (Vector2.Distance(enemy.transform.position, a.position) < range)
-        //    return enemy.transform;
-        //return null;
-        #endregion
         RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, 200, Vector2.zero, 0, LayerMask.GetMask("Enemy"));
 
         Transform result = null;
@@ -55,7 +44,7 @@ public class AutoTarget : MonoBehaviour
             Vector3 pos = transform.position;
             Vector3 targetPos = target.transform.position;
 
-            float dif = Vector3.Distance(pos, targetPos);
+            float dif = (transform.position - targetPos).sqrMagnitude;
             if (dif < diff)
             {
                 diff = dif;
